320e3039bf4bfe704f05b3eb9c687f96e454065c
Ranner198 <rancrump18@gmail.com>
2021-11-20 15:43:49 -0600

    Addeding https connectivity

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const path = require('path');
+var https = require('https');
+var fs = require('fs');
+
 
 const app = express();
 const port = 3000;
 
-// sendFile will go here
+const letsEncryptPath = "/etc/letsencrypt/live/rancrump.com"
+
+var options = {
+  key: fs.readFileSync(letsEncryptPath + 'privatekey.pem'),
+  cert: fs.readFileSync(letsEncryptPath + 'certificate.pem'),
+};
+
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/index.html'));
 });
 
-app.listen(port);
-console.log('Server started at http://localhost:' + port);
+var server = https.createServer(options, app).listen(port, function(){
+  console.log("Express server listening on port " + port);
+});
